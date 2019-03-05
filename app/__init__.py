b838e3bbc53a3bf2f6cd737a8a71a16d58fe86b3
mongkok <domake.io@gmail.com>
2019-03-06 06:21:34 +0700

    Wrapped api_call to app created

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import asyncio
 import os
 
-from flask import Flask, current_app
+from flask import Flask
 
 import flask_login
 import flask_slack
@@ -10,9 +10,6 @@ from flask_sslify import SSLify
 from slackclient import SlackClient
 
 from config import config
-
-from .api import api as api_blueprint
-from .main import main as main_blueprint
 
 login_manager = flask_login.LoginManager()
 slack_manager = flask_slack.SlackManager()
@@ -32,17 +29,21 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    from .api import api as api_blueprint  # isort:skip
+    from .main import main as main_blueprint  # isort:skip
+
     login_manager.init_app(app)
     slack_manager.init_app(app, blueprint=api_blueprint)
 
+    app.api_call = SlackClient(app.config['SLACK_API_TOKEN']).api_call
+
     @event_received.connect_via(app)
-    def slack_client(sender, data, **extra):
-        slack_api_token = sender.config['SLACK_API_TOKEN']
-        sender.api_call = SlackClient(slack_api_token).api_call
+    def event_logger(sender, data, **extra):
+        app.logger.debug(data['event'])
 
     @slack_manager.context_processor
     def context_processor(data):
-        return current_app.config['SLACK_EVENT_CONTEXT']
+        return app.config['SLACK_EVENT_CONTEXT']
 
     @slack_manager.dispatch_event_handler
     def async_event_dispatcher(sender, data, handlers, **extra):
